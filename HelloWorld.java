1abe0b39b2601db0b4df11cabc622a7a581adc00
Тихомиров Максим Витальевич <tvn1212yandex2@gmail.com>
2021-09-14 18:39:45 +0300

    *.class ignore added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-	System.out.println("B");
-	System.out.println("A");
-        System.out.println("Hello, World!12");
+        System.out.println("Hello, World!");
     }
 }
